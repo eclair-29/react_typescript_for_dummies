43021fe7a74241c656b039150d50a0fd76436976
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-15 16:28:09 +0800

    10. useState typing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,36 @@
-type FontSizes = 20 | 24 | 30 | 36;
+import { useState } from 'react';
 
-//.. START typescript lesson - 09. rest and spread
+type FontSizes = 20 | 24 | 30 | 36;
 
 type ButtonProps = {
   children: string[];
   // children: [string, string, string]; // ...tuple (length specific)
   heading?: FontSizes;
   styles: React.CSSProperties;
-  onBtnClick: (response: string) => string; // | void
-  defaultValue?: string;
+  onBtnClick?: (response: string) => string; // | void
+  defaultValue: string;
   btnId?: number;
 };
 
-const Button = ({ children, styles, onBtnClick, ...rest }: ButtonProps) => {
+//.. START typescript lesson - 10. useState typing
+
+type Students = {
+  name: string;
+  gradeLevel: number;
+  section: string;
+  gradeMarks: string[];
+  // ...
+};
+
+const Button = ({ children, styles, ...rest }: ButtonProps) => {
+  const [students, setStudents] = useState<Students[] | null>(null);
+
+  const getStudents = () => students;
+  // const studentSection = students && students[0]?.section;
+
   return (
     <button
-      onClick={() => onBtnClick('hi!')}
+      onClick={() => getStudents}
       style={styles}
       {...rest}
       className="pt-4 pb-3 px-6"
@@ -25,6 +40,6 @@ const Button = ({ children, styles, onBtnClick, ...rest }: ButtonProps) => {
   );
 };
 
-//.. END typescript lesson - 09. rest and spread
+//.. END typescript lesson - 10. useState typing
 
 export default Button;
