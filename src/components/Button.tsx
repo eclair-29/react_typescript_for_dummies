0f78c36c09e9fae8fb128cba78eb927a7b0bbf9b
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-16 10:37:47 +0800

    13. generics

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
+//.. START typescript lesson - 13. generics
 
-//.. START typescript lesson - 12. as type assertion
+// const convertToArray = <T,>(value: T): T[] => [value];
 
-type ButtonColor = 'red' | 'blue' | 'green';
+function convertToArray<T>(value: T): T[] {
+  return [value];
+}
+
+convertToArray(3);
+convertToArray('hello');
 
 const Button = () => {
-  useEffect(() => {
-    const previousBtnColor = localStorage.getItem('buttonColor') as ButtonColor;
-    // override the string | null ts inference to ButtonColor type
-  }), []);
-
   return <button className="pt-4 pb-3 px-6">Click</button>;
 };
 
-//.. END typescript lesson - 12. as type assertion
+//.. END typescript lesson - 13. generics
 
 export default Button;
